887b215d8df5dd52aa86bbb370b244b304bd0c76
Nathaniel F <nathaniel.fitzenrider@gmail.com>
2021-02-14 04:38:21 -0500

    Move to root directory and add license headers
    This was probably too much in 1 commit, but whatever.

---
+++ app/Main.hs
@@ -0,0 +1,10 @@
+{-
+ - SPDX-FileCopyrightText: 2020-2021 UnrelatedString <https://github.com/UnrelatedString> and other Perhaps contributors
+ -
+ - SPDX-License-Identifier: CC0-1.0
+ -}
+
+module Main where
+
+main :: IO ()
+main = putStrLn "No CLI yet -- use stack ghci to play around"
--- perhaps/app/Main.hs
+++
@@ -1,4 +0,0 @@
-module Main where
-
-main :: IO ()
-main = putStrLn "No CLI yet -- use stack ghci to play around"
